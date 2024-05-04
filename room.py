ae14f12b9d3ea897b12e932df80d8e618a5d5a64
MichalJ <rekwizytornia@hotmail.com>
2024-05-04 10:26:11 +0200

    zmiany w room

--- room.py
+++ room.py
@@ -4,30 +4,18 @@ def roll(min, max):
     return result
 
 class Room:
-    def __init__ (self, name, description, exit_n, exit_e, exit_s, exit_w):
+    def __init__ (self, name):
         self.name = name
-        self.exit_n = exit_n
-        self.exit_e = exit_e
-        self.exit_s = exit_s
-        self.exit_w = exit_w
-        self.description = description
+        self.exits = {}
+
+    def add_exits(self, diretion, next_room):
+        self.exits[direction] = next_room
 
     def look(self):
         print(self.name)
+        print(self.exits)
     
-    
-room = Room("Komnata 1", None, None, None, None, "Znajdujesz się w pierwszej komnacie. Są tu 4 wyścia.")
-
-room.look()
-
-def create_room():
-    room1 = Room("Komnata 2", None, None, None, None, "Znajdujesz się w drugiej komnacie. Są tu 4 wyścia.")
-    exits_roll = roll(1,3)
-    print(exits_roll)
-    return room1
-
-print(room.name)
-create_room()
-print(room1.name)
-
-exits = {"n": "polnoc", "e": "wschod", "s": "poludnie", "w": "zachod"}
+room1 = Room("Komnata 1")
+room2 = Room("Komnata 2")
+room3 = Room("Komnata 3")
+room4 = Room("Komnata 4")
